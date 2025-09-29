22bcfc03393316e613816c2561e27d04fa530eaa
kvpratama <kvpratama@gmail.com>
2025-09-29 18:58:41 +0900

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const NAV_ITEMS: { href: string; label: string }[] = [
+  {
+    href: "/",
+    label: "Dashboard",
+  },
+  {
+    href: "/search",
+    label: "Search",
+  },
+  {
+    href: "/watchlist",
+    label: "Watchlist",
+  },
+];
